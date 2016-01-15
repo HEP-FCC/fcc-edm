57320aa4995e5363f1a02e41c9338cbaa3bb0257
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-01-15 17:31:23 +0100

    Adding simpler read example

---
+++ examples/simpleread.cc
@@ -0,0 +1,64 @@
+// Data model
+#include "datamodel/EventInfo.h"
+#include "datamodel/EventInfoCollection.h"
+#include "datamodel/Particle.h"
+#include "datamodel/ParticleCollection.h"
+#include "datamodel/LorentzVector.h"
+
+// Utility functions
+#include "utilities/VectorUtils.h"
+
+// ROOT
+#include "TLorentzVector.h"
+#include "TBranch.h"
+#include "TFile.h"
+#include "TTree.h"
+
+// STL
+#include <iostream>
+#include <vector>
+
+// podio specific includes
+#include "podio/EventStore.h"
+#include "podio/ROOTReader.h"
+
+int main(){
+  std::cout<<"start processing"<<std::endl;
+
+  auto store = podio::EventStore();
+  auto reader = podio::ROOTReader();
+
+  reader.openFile("simpleexample.root");
+  store.setReader(&reader);
+  unsigned nevents= reader.getEntries();
+
+  for(unsigned iev=0; iev<nevents; ++iev) {
+    if(iev % 1000 == 0)
+      std::cout << "processing event " << iev << std::endl;
+
+    const EventInfoCollection* evinfocoll(nullptr);
+    bool evinfoPresent = store.get("evtinfo", evinfocoll);
+    if (evinfoPresent) {
+      auto evinfo = (*evinfocoll)[0];
+      if (iev < 10) {
+        std::cout << "read " << evinfo.Number() << std::endl;
+      }
+    }
+
+
+    const ParticleCollection* particles;
+    bool particlesPresent = store.get("mcparticles", particles);
+
+    if (particlesPresent) {
+      auto ptc = (*particles)[0];
+      if (iev < 10) {
+        std::cout << ptc.Core().Type << std::endl;
+        std::cout << ptc.Core().P4.Px << std::endl;
+      }
+    }
+
+    store.clear();
+    reader.endOfEvent();
+  }
+  return 0;
+}
