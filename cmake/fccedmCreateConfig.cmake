706d94e2c310eefc4da9a079fbd1ed721af1af7a
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-02-25 18:44:07 +0100

    Adding generation of targets file.

--- cmake/fccedmCreateConfig.cmake
+++ cmake/fccedmCreateConfig.cmake
@@ -7,3 +7,7 @@ write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/fccedmConfigVersion
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/fccedmConfig.cmake
               ${CMAKE_CURRENT_BINARY_DIR}/fccedmConfigVersion.cmake
         DESTINATION ${CMAKE_INSTALL_PREFIX}/cmake )
+
+#--- Generate targets file for downstream projects (included in pkg config) ----
+install(EXPORT fccedmTargets FILE fccedmTargets.cmake
+  DESTINATION ${INSTALL_LIB_DIR}/cmake)
