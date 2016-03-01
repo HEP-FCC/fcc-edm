434c4e9175a9681666686d5ff3e5e9e464c15081
Benedikt Hegner <hegner@cern.ch>
2016-03-01 21:27:48 +0100

    use LCG 83; don't override user-set PODIO

--- init.sh
+++ init.sh
@@ -10,11 +10,11 @@ if [[ "$unamestr" == 'Linux' ]]; then
 	#should check domain to make sure we're at CERN
 	#or is this software available somewhere in Lyon?
 	sw_afs=1
-	export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/2.8.9/Linux-i386/bin:${PATH}
-	source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
-	source /afs/cern.ch/exp/fcc/sw/0.6/LCG_80/ROOT/6.04.06/x86_64-slc6-gcc49-opt/bin/thisroot.sh
-	export PODIO=/afs/cern.ch/exp/fcc/sw/0.6/podio/0.2/x86_64-slc6-gcc49-opt/
-	echo software taken from /afs/cern.ch/sw/lcg
+        source /afs/cern.ch/sw/lcg/views/LCG_83/x86_64-slc6-gcc49-opt/setup.sh
+        if [ -z "$PODIO" ]; then
+            export PODIO=/afs/cern.ch/exp/fcc/sw/0.7/podio/snapshot/x86_64-slc6-gcc49-opt/
+        fi
+        echo software taken from /afs/cern.ch/sw/lcg
     fi
     export LD_LIBRARY_PATH=$FCCEDM/lib:$PODIO/lib:$LD_LIBRARY_PATH
 elif [[ "$unamestr" == 'Darwin' ]]; then
