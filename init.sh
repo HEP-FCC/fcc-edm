a2dafc4633cb563d352f70916b00e36bfaea5491
Joschka Lingemann <joschka.lingemann@cern.ch>
2017-02-21 10:22:07 +0100

    Move to cvmfs

--- init.sh
+++ init.sh
@@ -7,10 +7,10 @@ export FCCEDM=$PWD/install
 if [[ "$unamestr" == 'Linux' ]]; then
     platform='Linux'
     fs=$1
-    if [[ -d /afs/cern.ch/sw/lcg ]] && [[ `dnsdomainname` = 'cern.ch' ]] ; then
+    if [[ -d /cvmfs/fcc.cern.ch/sw ]] ; then
     #should check domain to make sure we're at CERN
     #or is this software available somewhere in Lyon?
-    source /afs/cern.ch/exp/fcc/sw/0.8/init_fcc_stack.sh $fs
+    source /cvmfs/fcc.cern.ch/sw/0.8/init_fcc_stack.sh $fs
     else
         if [ -z "$PODIO" ]; then
             echo "[ERROR] no podio setup detected, make sure it is setup"
